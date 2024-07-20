e9ad5f9113b10d2fbe191fb1baeec880cd884a18
indekwe <findekwe@gmail.com>
2024-07-20 04:18:08 -0700

    DOMcontentLoaded updated

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', (event)=>{
+document.addEventListener('DOMContentLoaded', function (event){
     const form=document.getElementById('registration-form')
     const feedbackDiv=document.getElementById('form-feedback')
     form.addEventListener('submit', event=>{
